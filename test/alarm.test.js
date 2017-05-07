a9f1df40c771e1a806a945023c739ad599109895
raydecastro <captray108@gmail.com>
2017-05-06 18:53:10 -0700

    feature. added an alarm test.

--- test/alarm.test.js
+++ test/alarm.test.js
@@ -1,10 +1,17 @@
+import { expect } from "chai";
 import Alarm from "../source/alarm";
 
-describe('Tire Pressure Monitoring System', () => {
-	describe('Alarm', () => {
-		it('shall do something', () => {
+describe("Tire Pressure Monitoring System", () => {
+	describe("Alarm", () => {
+		it("shall do something", () => {
 			const target = new Alarm();
 			target.check();
+		});
+
+		it("shall be off during initialization", () => {
+			const alarm = new Alarm();
+
+			expect(alarm.alarmOn()).to.be.false;
 		});
 	});
 });
---
+++ test/mockSensor.js
@@ -0,0 +1,5 @@
+class MockSensor {
+
+}
+
+export default MockSensor;
